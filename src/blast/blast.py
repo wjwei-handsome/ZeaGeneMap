2747254d3e463f7aa95b96e34528940fc3b6bb2e
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2022-07-19 22:59:44 +0800

    get rbh

---
+++ src/blast/blast.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import logging
+
+from ..utils.utils import search_file
+
+
+# def get_rbh_result(gene_name, rbh_df):
+#     result = []
+#     filter_df = rbh_df.query(f'qseqid_x=="{gene_name}"')
+#     if filter_df.empty:
+#         return 'None'
+#     else:
+#         for index, row in filter_df.iterrows():
+#             result.append(row['qseqid_y'])
+#         result_str = ','.join(result)
+#         return result_str
+
+
+def get_rbh_df(query_g, target_g, work_dir, e_filter: float = 1e-10) -> pd.DataFrame:
+    q_t_blast_fp, t_q_blast_fp = search_file(
+        work_dir, 'blast', query_g, target_g, type='rec')
+    print(
+        f'find:\n q_t_blast_fp: {q_t_blast_fp}\n t_q_blast_fp: {t_q_blast_fp}')
+    fwd_df = pd.read_csv(q_t_blast_fp, sep='\t', header=None)
+    rev_df = pd.read_csv(t_q_blast_fp, sep='\t', header=None)
+    column_names = ['qseqid', 'sseqid', 'pident', 'length', 'mismatch',
+                    'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
+    fwd_df.columns = column_names
+    rev_df.columns = column_names
+    fwd_df = fwd_df.drop_duplicates(subset=['qseqid'], keep='first')
+    rev_df = rev_df.drop_duplicates(subset=['qseqid'], keep='first')
+    rbh = pd.merge(fwd_df,
+                   rev_df[['qseqid', 'sseqid']],
+                   left_on='sseqid',
+                   right_on='qseqid',
+                   how='outer')
+    rbh = rbh.loc[rbh.qseqid_x == rbh.sseqid_y]
+    rbh = rbh.groupby(['qseqid_x', 'sseqid_x']).max().reset_index()[
+        ['qseqid_x', 'sseqid_x']]
+    rbh.columns = ['genename', 'target']
+    return rbh
