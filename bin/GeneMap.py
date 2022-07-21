72b25bd9e6764c20af86c223e7a9c55332c14263
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2022-07-21 23:53:11 +0800

    code struc

--- bin/GeneMap.py
+++ bin/GeneMap.py
@@ -9,20 +9,25 @@
 @邮箱        :wjwei9908@gmail.com
 '''
 
-# from blast.blast import get_rbh_result
 import argparse
 import logging
 import sys
-import pandas as pd
 from typing import *
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
+from src.utils import utils
+
+
+LEGAL_EVIDENCES = ['rbh', 'crossmap', 'synteny', 'ortholog']
 
 
 def get_args():
+    # define arguments
     parser = argparse.ArgumentParser(
         description=__doc__, prog='GeneMap.py')
+
+    # required arguments
     parser.add_argument('-l', '--list', action='store',
                         help='genename list file', required=True, dest='list')
     parser.add_argument('-d', '--dir', action='store',
@@ -31,71 +36,39 @@ def get_args():
                         help='query genome', required=True, dest='query_g')
     parser.add_argument('-t', '--target', action='store',
                         help='target genome', required=True, dest='target_g')
+    parser.add_argument('-o', '--output', action='store',
+                        help='output prefix', required=True, dest='output')
+
+    # options arguments
     parser.add_argument('-e', '--evidence',
                         help='evidence list', nargs='+', dest='evidence_l',
                         default=['rbh', 'crossmap', 'synteny', 'ortholog'])
-    # parser.add_argument('-d', '--dir', action='store_true',
-    #                     help='work dir', required=True)
+    parser.add_argument('-r', '--ratio', action='store',
+                        help='crossmap ratio', default=0.8, dest='ratio')
+    parser.add_argument('-p', '--overlap', action='store',
+                        help='overlap fraction', default=0.8, dest='overlap')
+
     return parser.parse_args()
 
 
-args = get_args()
+def main():
+    # get args
+    args = get_args()
 
-# judge the evidence
-evidence_list = args.evidence_l
-print(evidence_list)
-if any(e not in ['rbh', 'crossmap', 'synteny', 'ortholog'] for e in evidence_list):
-    logging.error('evidence error')
-    exit(1)
+    # judge the evidence
+    utils.check_in_list(args.evidence_l, LEGAL_EVIDENCES)
 
-# write result
-genelist_fp = args.list
+    # get raw genelist df
+    raw_df = utils.get_raw_df(args.list)
 
-raw_df = pd.read_csv(genelist_fp, sep='\t', header=None)
-assert raw_df.shape[1] == 1, 'genelist file format error'
-raw_df.columns = ['genename']
+    # add evidence and output raw df
+    raw_evd_df = utils.get_raw_evd_df(args, raw_df, write=True)
+
+    # process raw result
+    utils.process_raw(args, raw_evd_df)
 
 
-if 'rbh' in evidence_list:
-    from src.blast import blast
-    rbh_df = blast.get_rbh_df(args.query_g, args.target_g, args.dir)
-    raw_rbh_merge = pd.merge(
-        raw_df, rbh_df, on='genename', how='left')
-    output_df = raw_rbh_merge
-else:
-    output_df = raw_df
-    print('rbh not in evidence list')
-if 'ortholog' in evidence_list:
-    from src.ortholog import ortholog
-    ortholog_df = ortholog.get_ortholog_df(
-        args.query_g, args.target_g, args.dir)
-    raw_ortholog_merge = pd.merge(
-        output_df, ortholog_df, on='genename', how='left')
-    if len(raw_ortholog_merge) != len(raw_df):
-        exit(1)
-    output_df = raw_ortholog_merge
-else:
-    print('ortholog not in evidence list')
-if 'synteny' in evidence_list:
-    from src.synteny import synteny
-    rec_syn_df = synteny.get_rec_syn_df(
-        args.query_g, args.target_g, args.dir)
-    raw_syn_merge = pd.merge(
-        output_df, rec_syn_df, on='genename', how='left')
-    output_df = raw_syn_merge
-else:
-    print('synteny not in evidence list')
-if 'crossmap' in evidence_list:
-    from src.chain import crossmap
-    crossmap_df = crossmap.get_crossmap_df(
-        raw_df, args.query_g, args.target_g, args.dir)
-    raw_crossmap_merge = pd.merge(
-        output_df, crossmap_df, on='genename', how='left')
-    output_df = raw_crossmap_merge
-else:
-    print('crossmap not in evidence list')
+# TODO: 3） 修改print为log 4) 学会单元测试 5) 学会setup 6)
 
-output_df.fillna('None').to_csv(
-    'test_out.tsv', sep='\t', index=False, header=True)
-
-# TODO: 1）改进intersect的速度；2）增加ovp和ratio的cli参数传递；3）改进代码的结构; 4) 学会单元测试
+if __name__ == '__main__':
+    main()
